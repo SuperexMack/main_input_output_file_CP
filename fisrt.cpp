70dd133bdb9be9d5a7d47268f29ada48ab955ca8
Mack Walker <146699961+SuperexMack@users.noreply.github.com>
2024-03-12 05:09:56 +0530

    Update fisrt.cpp

--- fisrt.cpp
+++ fisrt.cpp
@@ -12,40 +12,4 @@ int main(){
 
 init_code();
 
-int t;
-cin>>t;
-while(t--){
-
-int n , m , k;
-
-cin>>n;
-cin>>m;
-cin>>k;
-
-int a[n];
-int b[m];
-
-for(int i = 0 ;i<n ; i++){
-	cin>>a[i];
 }
-
-for(int i = 0; i<m ; i++){
-	cin>>b[i];
-}
-
-int count = 0;
-
-for(int  i = 0 ; i<n ; i++){
-	for(int j = 0 ;j<m ; j++){
-		if(a[i] + b[j] <= k){
-         count++;
-		}
-	}
-}
-
-cout<<count<<endl;
-
-}
-
-}
-
